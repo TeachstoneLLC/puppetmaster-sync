5fb1d1a8e4a13197e099b75fcc6111399ac47e6d
perldork <perldork@webwizarddesign.com>
2015-02-27 12:19:21 -0500

    First rev of program + README

--- README.md
+++ README.md
@@ -15,8 +15,25 @@ Server code based on http://gilesbowkett.blogspot.com/2012/06/heroku-style-deplo
 
 # Pre-requisites
 
-* Working puppetmaster set up using multiple environments (https://docs.puppetlabs.com/puppet/latest/reference/environments_configuring.html)
-* Git / puppet repo set up with web hook configured to send pushes to
-  this process
+* Working puppetmaster set up using multiple environments (https://docs.puppetlabs.com/puppet/latest/reference/environments\_configuring.html)
+* Git / puppet repo set up with web hook configured to send pushes to this process
 * User set up on puppet master with SSH key for Github (deploy key) that allows it to do git pulls without human intervention
 * Modern version of ruby (version 2.0 or higher)
+* Webhook set up with secret configured on Github (put same on the client)
+  * Disable SSL verification as we use self-signed certs
+
+# Configuration
+
+* Ensure PUPPETMASTER\_SYNC\_SECRET is set to the Github secret set for the Github webservice hook
+* Ensure PUPPETMASTER\_SYNC\_CONFIG\_FILE is set to the fully qualified path of your YAML configuration file.
+
+## Configuration file
+
+Should have one key - branches - a hash, with the branch name as key and the directory as value. Example:
+
+```
+branches:
+  develop: /etc/puppet/develop
+  master:  /etc/puppet/master
+```
+
--- puppetmaster_sync.rb
+++ puppetmaster_sync.rb
@@ -1,21 +1,100 @@
 require 'rubygems'
+require 'logger'
 require 'sinatra'
 require 'json'
 
-class PuppetmasterSync
+class PuppetmasterSync < Sinatra::Base
 
-  def self.update?(json)
-    (JSON.parse(json)["ref"] == "refs/heads/master")
+  def initialize
+    @logger = ::Logger.new($stderr)
+    @branch_mappings = {}
+    @secret = nil
+    @config = parse_config_file
+    super
   end
 
-  def self.git_pull
-  `cd /project/directory && git checkout master && git pull origin master`
+  def parse_config_file
+    unless ENV.has_key? "PUPPETMASTER_SYNC_CONFIG_FILE"
+      raise RuntimeError.new("ENV missing 'PUPPETMASTER_SYNC_CONFIG_FILE' key")
+    end
+
+    config_file = ENV["PUPPETMASTER_SYNC_CONFIG_FILE"]
+
+    unless File.readable? config_file
+      raise RuntimeError.new("Configuration file #{config_file} is not readable")
+    end
+
+    YAML.load(config_file)
   end
 
-end
+  # Become puppet, cd to puppet checkout directory, check out the branch, update it
+  def update_puppetmaster_directory(branch, directory)
+    system %{su - puppet bash -c "cd #{directory} && git checkout #{directory} && git pull origin #{directory}"}
+  end
 
-set :port, 54321
+  # https://developer.github.com/webhooks/#payloads
+  #
+  # Github sends our configured secret as an HMAC hex digest of the payload, using the # hook’s secret 
+  # as the key (if configured).
 
-post '/' do
-  GitHubUpdater.git_pull if GitHubUpdater.update?(params[:payload])
+  configure do
+    set :app_file, __FILE__
+    set :dump_errors, true
+  end
+
+  before do
+    if !headers.has_key? "X-Hub-Signature"
+      @logger.error("Received request with no authorization (X-Hub-Signature header missing) - skipping")
+      halt 401
+    end
+
+    @logger.info("Processing Github delivery ID #{headers['X-Github-Delivery']}")
+
+    # From https://developer.github.com/webhooks/securing/
+    request.body.rewind
+    signature = "sha1=" + OpenSSL::HMAC.hexdigest(
+      OpenSSL::Digest.new("sha1"),
+      ENV["PUPPETMASTER_SYNC_SECRET"], request.body.read
+    )
+    unless Rack::Utils.secure_compare(signature, headers["X-Hub-Signature"])
+      halt 401, "Github push request - signature / secret mismatch: skipping"
+    end
+
+    request.body.rewind
+  end
+
+  # Happens when a push is done to the puppet-config repo
+  post '/' do
+
+    if !params.has_key? :payload
+      @logger.error("Received request with no payload - skipping")
+      halt 400
+    end
+
+    gh_webhook_response = JSON.parse(params[:payload])
+
+    unless gh_webhook_response.has_key? "pusher"
+      @logger.error("Received Github service hook response that is not of type 'push': skipping")
+      halt 400
+    end
+
+    branch = gh_webhook_response["ref"].split("/")[-1]
+
+    unless @config["branches"].has_key? branch
+      @logger.error("Received Github push service hook request for branch we don't monitor #{branch}: skipping")
+      halt 404, "Not watching for #{branch} updates"
+    end
+
+    update_puppetmaster_directory(branch, @config["branches"][branch])
+    @logger.info("updated #{branch}")
+
+    true
+  end
+
+  get '/' do
+    @logger.error("oops")
+  end
+
+  run! if $0 == __FILE__
+
 end
